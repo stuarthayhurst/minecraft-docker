1ec507e9e2bc03e96181efa31a10d4524ca9fd8e
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-05-12 03:07:48 +0100

    Move volumes into projects/

--- create-project.sh
+++ create-project.sh
@@ -5,7 +5,7 @@ if [[ "$PROJECT_NAME" == "" ]]; then
   PROJECT_NAME="minecraft-docker"
 fi
 
-mkdir -p "$PROJECT_NAME/data"
-mkdir -p "$PROJECT_NAME/config"
-mkdir -p "$PROJECT_NAME/plugins"
-mkdir -p "$PROJECT_NAME/logs"
+mkdir -p "projects/$PROJECT_NAME/data"
+mkdir -p "projects/$PROJECT_NAME/config"
+mkdir -p "projects/$PROJECT_NAME/plugins"
+mkdir -p "projects/$PROJECT_NAME/logs"
